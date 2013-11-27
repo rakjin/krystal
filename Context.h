8049bd9329e26febb8bec8531a76796808997b7e
rakjin <grampush@gmail.com>
2013-11-27 21:20:05 +0900

    DOC: add context table descriptions, etc

--- Context.h
+++ Context.h
@@ -22,4 +22,36 @@ namespace Rakjin {
 
 }
 
+
+/* * * * * * * * * * * * * * * * * * * * 
+ * Aaa.kst:
+ *  #include Bbb.kst
+ *  packet A { B b; };
+ * 
+ * Bbb.kst:
+ *  #include Ccc.kst
+ *  packet B { C c; };
+ * 
+ * Ccc.kst:
+ *  packet C { int i; };
+ * 
+ * this should work:
+ * $krystal -cs Aaa.kst > Aaa.cs
+ * and hash change of packet C
+ * should influence all the others' hash
+ * even when processing Aaa.kst solely
+ *
+ * Included Files Table
+ *   map <string, bool>* includedFiles;
+ *     - <name_of_files_to_be_processed, is_processed>
+ *     - including Aaa.kst as first element
+ * Declarations Table
+ *   map<string, int>* declarations;
+ *     - <name_of_declarations, hash>
+ *     - 0 hash means not yet been calculated
+ * 
+ * * * * * * * * * * * * * * * * * * * */
+
+
+
 #endif // CONTEXT_H
